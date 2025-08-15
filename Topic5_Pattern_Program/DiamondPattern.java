bdcae79b955e8da30eb8267dcd4778ae0474287b
Arpita155 <arpitasahoo795@gmail.com>
2025-08-16 00:55:01 +0530

    Create DiamondPattern.java

---
+++ Topic5_Pattern_Program/DiamondPattern.java
@@ -0,0 +1,35 @@
+package Sanfoundry_for_questions.Topic5_Pattern_program;
+
+import java.util.Scanner;
+
+public class DiamondPattern {
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the length:");
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<(n-i-1);j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<(i*2+1);j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<(n-i-1);j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<(n*2-(i*2+1));j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
